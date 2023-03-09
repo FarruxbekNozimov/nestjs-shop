ccb2a6c3a8a091e0e43e4a7a4334397ba66d05fa
FarruxDEV <farruxbeknozimov@gmail.com>
2023-03-09 11:11:43 +0500

    Connect to swagger

--- src/equipment/equipment.controller.ts
+++ src/equipment/equipment.controller.ts
@@ -6,31 +6,38 @@ import {
   Patch,
   Param,
   Delete,
+  Put,
 } from '@nestjs/common';
 import { EquipmentService } from './equipment.service';
 import { CreateEquipmentDto } from './dto/create-equipment.dto';
 import { UpdateEquipmentDto } from './dto/update-equipment.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Equipments')
 @Controller('equipment')
 export class EquipmentController {
   constructor(private readonly equipmentService: EquipmentService) {}
 
+  @ApiOperation({ summary: 'Create equipment' })
   @Post()
   create(@Body() createEquipmentDto: CreateEquipmentDto) {
     return this.equipmentService.create(createEquipmentDto);
   }
 
+  @ApiOperation({ summary: 'Get all equipments' })
   @Get()
   findAll() {
     return this.equipmentService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get one equipment' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.equipmentService.findOne(+id);
   }
 
-  @Patch(':id')
+  @ApiOperation({ summary: 'Update equipment' })
+  @Put(':id')
   update(
     @Param('id') id: string,
     @Body() updateEquipmentDto: UpdateEquipmentDto,
@@ -38,6 +45,7 @@ export class EquipmentController {
     return this.equipmentService.update(+id, updateEquipmentDto);
   }
 
+  @ApiOperation({ summary: 'Delete equipment' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.equipmentService.remove(+id);
--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -6,35 +6,43 @@ import {
   Patch,
   Param,
   Delete,
+  Put,
 } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@Controller('order')
+@ApiTags('Orders')
+@Controller('orders')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
+  @ApiOperation({ summary: 'Create order' })
   @Post()
   createOrder(@Body() createOrderDto: CreateOrderDto) {
     return this.orderService.createOrder(createOrderDto);
   }
 
+  @ApiOperation({ summary: 'Get all orders' })
   @Get()
   findAll() {
     return this.orderService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get one order' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.orderService.findOne(+id);
   }
 
-  @Patch(':id')
+  @ApiOperation({ summary: 'Update order' })
+  @Put(':id')
   updateOrder(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.orderService.updateOrder(+id, updateOrderDto);
   }
 
+  @ApiOperation({ summary: 'Delete order' })
   @Delete(':id')
   deleteOrder(@Param('id') id: string) {
     return this.orderService.deleteOrder(+id);
